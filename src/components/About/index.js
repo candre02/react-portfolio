a69fa8d706b73cf0fd1992ccf8db630c616c27f7
Crystal Andre <81876258+candre02@users.noreply.github.com>
2022-02-16 15:37:20 -0600

    about file: added id: A1 for styling which added padding, id: greeneye for styling: height, border-radius, padding, changed img tag classnam rounded-circle, added id:GE2 changed fontfamily and font size

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,9 +6,9 @@ import { Container } from "react-bootstrap";
 function About() {
     return (
         <Container>
-            <h1>About Me</h1>
-            <img src={about} className="img-fluid" alt="about" id="greeneye"/>
-            <p>
+            <h1 id="A1">About Me</h1>
+            <img src={about} className="rounded-circle" alt="about"  id="greeneye"/>
+            <p id="GE2">
                 Hi, I'm Crystal Andre and I'm a Full Stack Web Developer. I'm a problem solver by nature, who loves learning the why behind all things technical. 
                 In my past career, I spent over a decade working as a mechanic: first on cars, then doing more complex electrical work on forklifts and diesel trucks. 
                 I love learning new ways to improve processes and become more efficient, and I'm passionate about delivering the best possible outcomes for my team and end client. 
