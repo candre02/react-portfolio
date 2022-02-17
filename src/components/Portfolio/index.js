2bfeb5981376b0d467ec9d0fe2ec007cbe36ba5e
Crystal Andre <81876258+candre02@users.noreply.github.com>
2022-02-16 18:25:30 -0600

    added id:pf1 in portfolio file for styling, each card added more stying outline, marging right or left or top/bottom, padding, position, changed the font family for both title and text

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,16 +11,22 @@ function Portfolio() {
     return (
         <Container>
             <div>
-                <h1>Portfolio</h1>
+                <h1 id="PF1">Portfolio</h1>
             </div>
                 <CardGroup>
                     <div className="bg-light border">
-                        <Card style={{ width: '18rem'}}>
+                        <Card style={{ 
+                            width: '18rem', 
+                            outline: '1px solid white',
+                            marginRight:'10px',
+                            padding: '14px',
+                            position: 'absolute'
+                            }}>
                             <a href="https://candre02.github.io/Fundamental-Quiz/">
                                 <Card.Img variant="top" src={Quiz} />
                             </a>
                             <Card.Body>
-                                <Card.Title>Fundamental Quiz</Card.Title>
+                                <Card.Title id="pf2">Fundamental Quiz</Card.Title>
                                 <Card.Text>
                                 Quiz on javascript fundamentals, included Q/A's, timer is deducted when answers are wrong, 
                                 presented applicants initials and there total score. Check out Fundamental Quiz by clicking on the image or Github repository click{" "}
@@ -30,12 +36,18 @@ function Portfolio() {
                         </Card>
                     </div>
                     <div className="bg-light border">
-                    <Card style={{ width: '18rem'}}>
+                    <Card style={{ 
+                        width: '18rem',
+                        outline: '1px solid white',
+                        marginLeft: '957px',
+                        padding: '14px',
+                        position: 'absolute'
+                        }}>
                             <a href="https://github.com/candre02/Profile-Building.git">
                                 <Card.Img variant="top" src={TB} />
                             </a>
                             <Card.Body>
-                                <Card.Title>Profile Building</Card.Title>
+                                <Card.Title id="pf2">Profile Building</Card.Title>
                                 <Card.Text>
                                     This is a Object-Oriented Programming (OOP) Project, that prompts infomation about each team member employed.
                                     Check out Profile Building by clicking on the image or Github repository click{" "}
@@ -45,12 +57,17 @@ function Portfolio() {
                         </Card>
                     </div>
                     <div className="bg-light border">
-                    <Card style={{ width: '18rem'}}>
+                    <Card style={{ 
+                        width: '18rem',
+                        outline: '1px solid white',
+                        marginLeft: '478px',
+                        padding: '8px'
+                        }}>
                             <a href="https://github.com/candre02/Employee-Guide.git">
                                 <Card.Img variant="top" src={ET} />
                             </a>
                             <Card.Body>
-                                <Card.Title>Employee Guide</Card.Title>
+                                <Card.Title id="pf2">Employee Guide</Card.Title>
                                 <Card.Text>
                                     This is a SQL Project, allows the user to track employees by using there department's, role's, title's, saleries and manage their manager status.
                                     Check out Profile Building by clicking on the image or Github repository click{" "}
@@ -60,12 +77,18 @@ function Portfolio() {
                         </Card>
                     </div>
                     <div>
-                    <Card style={{ width: '18rem'}}>
+                    <Card style={{ 
+                        width: '18rem',
+                        outline: '1px solid white',
+                        padding: '10px',
+                        marginTop: '45px',
+                        marginLeft: '18%',
+                        }}>
                             <a href="https://github.com/candre02/digital-TradeBEnd.git">
                                 <Card.Img variant="top" src={EC} />
                             </a>
                             <Card.Body>
-                                <Card.Title>digital-TradeBEnd</Card.Title>
+                                <Card.Title id="pf2">digital-TradeBEnd</Card.Title>
                                 <Card.Text>
                                     This is a Object-Relational Mapping (ORM) Project, allows the user to create, read, update, and delete (CRUD) on the related material routes.
                                     Check out digital-TradeBEnd by clicking on the image or Github repository click{" "}
@@ -75,12 +98,19 @@ function Portfolio() {
                         </Card>
                     </div>
                     <div>
-                    <Card style={{ width: '18rem'}}>
+                    <Card style={{ 
+                        width: '18rem',
+                        outline: '1px solid white',
+                        padding: '6px',
+                        marginLeft: '60%',
+                        marginBottom: '320px',
+                        marginTop: '-480px'
+                        }}>
                             <a href="https://secret-forest-66148.herokuapp.com/">
                                 <Card.Img variant="top" src={GS} />
                             </a>
                             <Card.Body>
-                                <Card.Title>Git-Stuff</Card.Title>
+                                <Card.Title id="pf2">Git-Stuff</Card.Title>
                                 <Card.Text>
                                 An online database in which products are stored. The user creates a listing, sets a price and gives a short description of the product. 
                                 Users emails are displayed so that other users can contact them in regards to the product.
